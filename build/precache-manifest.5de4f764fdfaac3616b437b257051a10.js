aa1657652ba0aa98055f68768cf5925521c0136d
Yehong Piao <piaoyehong1107@gmail.com>
2020-10-03 21:05:18 +0000

    Done.

---
+++ build/precache-manifest.5de4f764fdfaac3616b437b257051a10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baa720709d6b70bed4a4f0cb0fa4c353",
+    "url": "/index.html"
+  },
+  {
+    "revision": "927fbfa44be98a9afa1c",
+    "url": "/static/js/2.97b4407a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.97b4407a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0dbf45c0aede4dc4050",
+    "url": "/static/js/main.e78b908e.chunk.js"
+  },
+  {
+    "revision": "b6b7d9f341d802461e6f",
+    "url": "/static/js/runtime-main.b2d6c529.js"
+  }
+]);
